134231102ae6cbae485256f408260083bc1ce463
mplouffe <matheuplouffe@gmail.com>
2018-08-02 15:52:08 -0500

    Split the routes out into their own components and set up the router AppRouter.

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component In the New File.
+    </div>
+);
+
+export default ExpenseDashboardPage;

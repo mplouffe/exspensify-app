63625217bd1b98f6ae3abe365d83ec927e4862ad
mplouffe <matheuplouffe@gmail.com>
2018-08-03 19:42:27 -0500

    Added controlled components to the project.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter, } from './actions/filters';
-import getVisibleExpenses  from './selectors/expenses'
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import { sortByAmount } from './actions/filters';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', note: 'For last of March.', amount: 500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', note: 'For last of March.', amount: 600 }));
-store.dispatch(setTextFilter('wateR'));
-const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
-console.log(store.getState());
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(addExpense({ description: 'Water Bill', note: 'For last of March.', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', note: 'For last of March.', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+store.dispatch(sortByAmount());
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+ReactDOM.render(jsx, document.getElementById('app'));

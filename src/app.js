e0c9f302ae7e14018d3558db339f2f49da427d0d
mplouffe <matheuplouffe@gmail.com>
2018-08-05 00:04:05 -0500

    Added production settings for Webpack and CSS bundling and mapping.

--- src/app.js
+++ src/app.js
@@ -4,19 +4,13 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import { sortByAmount } from './actions/filters';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', note: 'For last of March.', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', note: 'For last of March.', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-store.dispatch(sortByAmount());
 const jsx = (
     <Provider store={store}>
         <AppRouter />
